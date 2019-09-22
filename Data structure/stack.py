b6522f6536737821bc5eea264d235f1e1e07034c
SANYAM1996 <sanyamsingh.96@gmail.com>
2019-09-22 18:46:39 +0530

    converting interger into binary

--- Data structure/stack.py
+++ Data structure/stack.py
@@ -24,3 +24,33 @@ print(s.get_stacks())
 s.peek()
 print (s.get_stacks())
 print(s.peek())
+# converting integer into binary by stack data structure
+
+
+class Stack:
+
+    def __init__(self):
+        self.stack=[]
+    def push(self,data):
+        return self.stack.append(data)
+    def pop(self):
+        return self.stack.pop()
+    def top(self):
+        return self.stack[-1]
+    def emp(self):
+        if self.stack==[]:
+            return True
+        else:
+            return False
+        def get(self):
+
+            return self.stack
+def convert(stack,num):
+    while num>0:
+        c=num
+        bina=c%2
+        stack.push(bina)
+        num//=2
+    return stack.get()
+stack=Stack()
+print(convert(stack,27))
