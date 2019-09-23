87159184e06393511996be6fb04811865c7679cd
Aman <aman.endeavor@gmail.com>
2019-09-23 16:05:10 +0100

    evaluate

---
+++ VS CODE/Evaluation.py
@@ -0,0 +1,7 @@
+# Write functions for the following tasks
+
+# 1. function to read and print head of train.csv
+
+# 2. function to read and print column names from train.csv
+
+# 3. function to read and print data shape from train.csv
